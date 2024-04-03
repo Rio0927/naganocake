f2e31ad7aa591443b6cf07858c1081103ddc6f63
EC2 Default User <ec2-user@ip-172-31-58-113.ec2.internal>
2024-04-03 06:49:49 +0000

    add bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5b8aaf4e15e744c8a10ed795adbd36cb.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,69 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  
+  namespace :admin do
+    get 'homes/top'
+    
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    patch 'customers/update'
+    
+    get 'genres/index'
+    post 'genres/create'
+    get 'genres/edit'
+    patch 'genres/update'
+    
+    get 'items/index'
+    get 'items/new'
+    post 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    patch 'items/update'
+    
+    patch 'order_details/update'
+    
+    get 'orders/show'
+    patch 'orders/update'
+  end
+
+  namespace :public do
+    get 'homes/about'
+    
+    get 'orders/new'
+    post 'orders/confirm'
+    get 'orders/thanks'
+    post 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+    
+    get 'items/index'
+    get 'items/show'
+    
+    get 'cart_items/index'
+    patch 'cart_items/update'
+    delete 'cart_items/destroy'
+    delete 'cart_items/destroy_all'
+    post 'cart_items/create'
+    
+    get 'addresses/index'
+    get 'addresses/edit'
+    post 'addresses/create'
+    patch 'addresses/update'
+    delete 'addresses/destroy'
+  end
+  
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
